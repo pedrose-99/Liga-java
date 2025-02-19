1545455cd42eaae501ef69f611d3d6dbb5933e71
alvaro <alvaromunozqd@gmail.com>
2025-02-19 12:10:49 +0100

    cc

--- Liga/src/Humano/Persona.java
+++ Liga/src/Humano/Persona.java
@@ -14,5 +14,40 @@ public class Persona
         this.edad = edad;
         this.continente = continente;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Continente getContinente() {
+        return continente;
+    }
+
+    public void setContinente(Continente continente) {
+        this.continente = continente;
+    }
+
     
+    
+
 }
