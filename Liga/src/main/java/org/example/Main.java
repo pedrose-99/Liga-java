7db0f397e119e45737e06df40bcd9c4365a22814
Pedro <pedserra.99@gmail.com>
2025-02-20 14:11:45 +0100

    Generador de nombres

--- Liga/src/main/java/org/example/Main.java
+++ Liga/src/main/java/org/example/Main.java
@@ -1,15 +1,37 @@
 package org.example;
 import com.github.javafaker.*;
 
+import java.util.Locale;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		Faker faker = new Faker();
 
-		String firstName = faker.name().firstName(); // Emory
-		String lastName = faker.name().lastName(); // Barton
+		Faker fakerEs = new Faker(new Locale("es"));
+		Faker fakerEn = new Faker(Locale.ENGLISH);
+		Faker fakerCh = new Faker(new Locale("hi", "IN"));
+		Faker fakerAF = new Faker(new Locale("en", "NG"));
 
-		System.out.println(firstName + " " + lastName);
+
+		String firstName = fakerEs.name().firstName();
+		String lastName = fakerEs.name().lastName();
+
+		System.out.println("Nombre español: "+ firstName + " " + lastName);
+
+		firstName = fakerEn.name().firstName();
+		lastName = fakerEn.name().lastName();
+		
+		System.out.println("Nombre Ingles: "+ firstName + " " + lastName);
+
+		firstName = fakerCh.name().firstName();
+		lastName = fakerCh.name().lastName();
+		
+		System.out.println("Nombre Indio: "+ firstName + " " + lastName);
+
+		firstName = fakerAF.name().firstName();
+		lastName = fakerAF.name().lastName();
+		
+		System.out.println("Nombre Africano: "+ firstName + " " + lastName);
 	}
 }
