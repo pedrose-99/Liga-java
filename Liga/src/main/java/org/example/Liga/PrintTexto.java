f479b877036b9c32de36869baeec38339a2d70ab
alvaro <alvaromunozqd@gmail.com>
2025-03-02 18:30:41 +0100

    cambios en colores

--- Liga/src/main/java/org/example/Liga/PrintTexto.java
+++ Liga/src/main/java/org/example/Liga/PrintTexto.java
@@ -1,6 +1,7 @@
 package org.example.Liga;
 
 public class PrintTexto {
+    // Códigos de colores ANSI
     public static final String RED = "\033[31m"; 
     public static final String GREEN = "\033[32m";
     public static final String YELLOW = "\033[33m";
@@ -8,7 +9,11 @@ public class PrintTexto {
     public static final String PURPLE = "\033[35m"; 
     public static final String CYAN = "\033[36m"; 
     public static final String WHITE = "\033[37m";
-    public static final String RESET = "\033[0m";
+    public static final String RESET = "\033[0m";  // Restablecer color al original
+
+    // Estilos de texto adicionales
+    public static final String BOLD = "\033[1m";         // Texto en negrita
+    public static final String UNDERLINE = "\033[4m";    // Texto subrayado
 
 }
 
